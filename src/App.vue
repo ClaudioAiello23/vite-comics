9b8459268562a8d5d1b301ae21f13a58e5820365
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-23 15:10:59 +0100

    sistemazione struttura base Vite - Vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<!-- PARTE VUE JS-->
+<script>
+// import dei files dalla cartella components
+// import dei files dalla cartella components
+export default {
+  components: {
+    // aggiunta files dall'oggetto components 
+    // aggiunta files dall'oggetto components
+  }
+}
 </script>
 
+<!-- PARTE HTML -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppTitle />
+  <AppHero />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<!-- PARTE CSS -->
+<style scoped></style>
