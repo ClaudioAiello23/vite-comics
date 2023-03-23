5c4f6bf3826b9d48898373a8de00e53c59057d96
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-23 15:53:36 +0100

    Header: array objects nav menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,16 +3,61 @@
 export default {
     data() {
         return {
-            name: 'AppHeader'
-        }
+            name: 'AppHeader',
+            headerMenu: [
+                {
+                    menuName: 'CHARACTERS'
 
+                },
+                {
+                    menuName: 'COMICS'
+
+                },
+                {
+                    menuName: 'MOVIES'
+
+                },
+                {
+                    menuName: 'TV'
+
+                },
+                {
+                    menuName: 'GAMES'
+
+                },
+                {
+                    menuName: 'COLLECTIBLES'
+
+                },
+                {
+                    menuName: 'VIDEOS'
+
+                },
+                {
+                    menuName: 'FANS'
+
+                },
+                {
+                    menuName: 'NEWS'
+
+                },
+                {
+                    menuName: 'SHOP'
+
+                },
+            ]
+        }
     }
 }
 </script>
 
 <!-- PARTE HTML -->
 <template>
-    HEADER
+    <nav>
+        <ul>
+            <li>{{ headerMenu[0].menuName }}</li>
+        </ul>
+    </nav>
 </template>
 
 <!-- PARTE SCSS-->
