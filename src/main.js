1816f1347edd806dfca9acfca40b149da11d1147
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-24 18:42:29 +0100

    aggiunta Google Font - Montserrat

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/styles/main.scss'
+import "@fontsource/montserrat"; // aggiunto Google fonts dopo installazione da npm
 import App from './App.vue'
 
 
